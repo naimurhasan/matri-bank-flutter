baa7ae48cd4cb0c54641daf9e76282d2bd0b21ec
Naimur Hasan <37500414+naimurhasan@users.noreply.github.com>
2023-04-05 09:37:45 +0600

    deleting unnecessary files

--- lib/main.dart
+++ lib/main.dart
@@ -2,7 +2,6 @@ import 'package:flutter/material.dart';
 import 'package:flutter_bloc_sample/bloc/domain/login/login_bloc.dart';
 import 'package:flutter_bloc_sample/bloc/presentation/home_screen.dart';
 import 'package:flutter_bloc_sample/bloc/presentation/splash_screen.dart';
-import 'package:flutter_bloc_sample/bloc/presentation/user_details_screen.dart';
 import 'package:flutter_bloc_sample/bloc/presentation/widgets/login_screen.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 
